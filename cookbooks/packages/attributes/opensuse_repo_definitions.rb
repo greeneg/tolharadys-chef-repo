1f8a970e82bda5fce9cfd00cfd149d5564691b50
Gary Greene <greeneg@tolharadys.net>
2023-09-17 19:01:39 -0400

    Fix up config for virtualization repos
    Signed-off-by: Gary Greene <greeneg@tolharadys.net>

--- cookbooks/packages/attributes/opensuse_repo_definitions.rb
+++ cookbooks/packages/attributes/opensuse_repo_definitions.rb
@@ -925,20 +925,12 @@ default['packages']['opensuse']['15.4']['repo']['obs_-_Utilities'] = {
     'url' => 'https://download.opensuse.org/repositories/utilities/15.4/',
     'file_name' => 'obs_-_Utilities'
 }
-default['packages']['opensuse']['15.4']['repo']['obs_-_Virtualization:Containers'] = {
-    'autorefresh' => false,
-    'enabled' => true,
-    'keeppackages' => false,
-
-    'priority' => 90,
-    'type' => 'rpm-md',
-    'url' => 'https://download.opensuse.org/repositories/Virtualization:/containers/15.4/',
-    'file_name' => 'obs_-_Virtualization:Containers'
-}
 default['packages']['opensuse']['15.4']['repo']['obs_-_Virtualization'] = {
     'autorefresh' => false,
+    'description' => 'OBS - Virtualization',
     'enabled' => true,
     'gpgcheck' => true,
+    'gpgautoimportkeys' => true,
     'gpgkey' => 'https://download.opensuse.org/repositories/Virtualization/15.4/repodata/repomd.xml.key',
     'keeppackages' => false,
     'priority' => 90,
@@ -946,10 +938,25 @@ default['packages']['opensuse']['15.4']['repo']['obs_-_Virtualization'] = {
     'url' => 'https://download.opensuse.org/repositories/Virtualization/15.4/',
     'file_name' => 'obs_-_Virtualization'
 }
-default['packages']['opensuse']['15.4']['repo']['obs_-_Virtualization:Vagrant'] = {
+default['packages']['opensuse']['15.4']['repo']['obs_-_Virtualization:Containers'] = {
     'autorefresh' => false,
+    'description' => 'OBS - Virtualization:Containers',
     'enabled' => true,
     'gpgcheck' => true,
+    'gpgautoimportkeys' => true,
+    'gpgkey' => 'https://download.opensuse.org/repositories/Virtualization/containers/15.4/repodata/repomd.xml.key',
+    'keeppackages' => false,
+    'priority' => 90,
+    'type' => 'rpm-md',
+    'url' => 'https://download.opensuse.org/repositories/Virtualization:/containers/15.4/',
+    'file_name' => 'obs_-_Virtualization:Containers'
+}
+default['packages']['opensuse']['15.4']['repo']['obs_-_Virtualization:Vagrant'] = {
+    'autorefresh' => false,
+    'description' => 'OBS - Virtualization:Vagrant',
+    'enabled' => true,
+    'gpgcheck' => true,
+    'gpgautoimportkeys' => true,
     'gpgkey' => 'https://download.opensuse.org/repositories/Virtualization:/vagrant/15.4/repodata/repomd.xml.key',
     'keeppackages' => false,
     'priority' => 90,
@@ -959,8 +966,10 @@ default['packages']['opensuse']['15.4']['repo']['obs_-_Virtualization:Vagrant'] 
 }
 default['packages']['opensuse']['15.4']['repo']['obs_-_Virtualization:VirtualBox'] = {
     'autorefresh' => false,
-    'enabled' => false,
+    'description' => 'OBS - Virtualization:VirtualBox',
+    'enabled' => true,
     'gpgcheck' => true,
+    'gpgautoimportkeys' => true,
     'gpgkey' => 'https://download.opensuse.org/repositories/Virtualization:/virtualbox/15.4/repodata/repomd.xml.key',
     'keeppackages' => false,
     'priority' => 90,
